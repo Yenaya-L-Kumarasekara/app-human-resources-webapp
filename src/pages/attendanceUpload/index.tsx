8f9b7df186586568fb159a19ce4dbfdf110c1f62
Amila T Kumarasekara <91652718+Yenaya-L-Kumarasekara@users.noreply.github.com>
2024-01-07 14:36:14 +0530

    Design and develop the functionality to upload the data to the server

---
+++ src/pages/attendanceUpload/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import * as XLSX from 'xlsx';
+import axios from 'axios';
+import { convertSerialDateToFormattedString } from '../../utils/attendanceUtils';
+
+
+const AttendanceUpload = () => {
+  const uploadDataToServer = async (data: any) => {
+    try {
+      console.log(data);
+      const res = await axios.post(`${process.env.REACT_APP_AXIOS_API_URL}/uploadAttendance`, {attendance_data : data});
+      console.log(res);
+      alert('Attendance data uploaded successfully.');
+    } catch (err) {
+      console.error(err);
+      alert('Error uploading attendance data.');
+    }
+  };
+
+  const readUploadFile = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const fileInput = e.currentTarget.querySelector('#upload');
+
+    if (fileInput instanceof HTMLInputElement) {
+      const file = fileInput.files?.[0];
+
+      if (file) {
+        const allowedFileTypes = ['application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'];
+        const fileType = file.type;
+
+        if (allowedFileTypes.includes(fileType)) {
+          const chunkSize = 1024 * 1024; // 1MB
+          let offset = 0;
+          const reader = new FileReader();
+  
+          const processChunk = (chunk: ArrayBuffer) => {
+            const workbook = XLSX.read(chunk, { type: 'array' });
+            const sheetName = workbook.SheetNames[0];
+            const worksheet = workbook.Sheets[sheetName];
+            const json = XLSX.utils.sheet_to_json(worksheet);
+
+            // Convert serial dates to Date objects
+            const processedData = json.map((entry: any) => {
+              const checkInDate = convertSerialDateToFormattedString(entry.check_in);
+              const checkOutDate = convertSerialDateToFormattedString(entry.check_out);
+
+              return {
+                employee_id: entry.employee_id,
+                check_in: checkInDate,
+                check_out: checkOutDate,
+              };
+            });
+            uploadDataToServer(processedData);
+          };
+  
+          const readNextChunk = () => {
+            const chunk = file.slice(offset, offset + chunkSize);
+            reader.onload = (e) => {
+              const target: any = e.target;
+              const chunkData: any = target.result;
+              processChunk(chunkData);
+              offset += chunkSize;
+              if (offset < file.size) {
+                readNextChunk();
+              }
+            };
+            reader.readAsArrayBuffer(chunk);
+          };
+  
+          readNextChunk();
+        } else {
+          console.error('Invalid file type.');
+          alert('Invalid file type.');
+        }
+      } else {
+        console.error('No file selected.');
+        alert('No file selected.');
+      }
+    }
+  };
+
+  return (
+    <div className="flex justify-center min-h-screen">
+      <div className="mb-3 w-96">
+        <form className="mb-3" onSubmit={readUploadFile}>
+          <label htmlFor="upload" className="inline-block mb-2 text-gray-700">
+            Upload the Attendance sheet
+          </label>
+          <input type="file" id="upload" />
+          <button type="submit">Upload</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AttendanceUpload;
