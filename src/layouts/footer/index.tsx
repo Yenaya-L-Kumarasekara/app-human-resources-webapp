230aa83b415d99707563553415c309f0ea690f46
Amila T Kumarasekara <91652718+Yenaya-L-Kumarasekara@users.noreply.github.com>
2024-01-07 14:32:44 +0530

    Design the footer for the app human resources web app

---
+++ src/layouts/footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        
+        <footer className="p-4 bg-white flex items-center justify-between dark:bg-gray-900">
+            <span className="text-sm text-gray-500 text-center dark:text-gray-400">
+                Human Resources
+            </span>
+        </footer>
+
+    );
+}
+
+export default Footer;
